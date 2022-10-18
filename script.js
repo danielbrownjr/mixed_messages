7ccf57e2bf0fe3dc384ff75f7ea5cf2aaf2f5518
Daniel Brown <daniel.brown@hey.com>
2022-10-18 07:28:43 -0400

    Updated gitignore to hide local scratch pad

---
+++ script.js
@@ -0,0 +1,12 @@
+const testFunction = (a = 0, b = 0) => { return a+b };
+
+const letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const symbolArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '?', '/'];
+
+const numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const astrologicalSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+
